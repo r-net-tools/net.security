68b9c56ade372f74a2e1082cf85871e5d2919218
Humbert <humbert.costas@gmail.com>
2016-06-25 23:33:44 +0200

    GetRIPE.inet returns RIPE inet-num as data frame
    Former-commit-id: 3e4f1d0b8eac2936bc2b995c289f6019065f03a6

--- R/ripe.R
+++ R/ripe.R
@@ -1,7 +1,7 @@
 #' GetRIPE.ASN
 #'
 #' @param path where RIPE files will be downloaded and unziped (don't finish with /). Default set as inst/tmpdata
-#' @param download TRUE if you want to update the source data, default set as FALSE#'
+#' @param download TRUE if you want to update the source data, default set as FALSE
 #' @return data frame
 #' @export
 #'
@@ -9,10 +9,12 @@
 #' ripe.asn <- GetRIPE.ASN()
 #' ripe.asn <- GetRIPE.ASN(download = TRUE)
 GetRIPE.ASN <- function(path = "inst/tmpdata", download = FALSE){
-  #
+  # Download and read raw data
   path <- ifelse(download, DownloadRIPE.ASN(path), paste(path, "ripe/ripe.db.aut-num", sep = "/"))
   ripe.asn <- read.delim(file = path, sep = ":", header = FALSE, skip = 6,
                          quote = "\"", stringsAsFactors = FALSE)
+
+  # Raw data to filtered data frame
   ripe.asn <- ripe.asn[ripe.asn$V1 %in% c("aut-num","as-name","descr","admin-c","tech-c","mnt-by","source","org","status","created","last-modified"),]
   ripe.asn$V1 <- as.factor(ripe.asn$V1)
   ripe.asn.lite <- ripe.asn[ripe.asn$V1 %in% c("aut-num","as-name","source","status"),]
@@ -24,9 +26,51 @@ GetRIPE.ASN <- function(path = "inst/tmpdata", download = FALSE){
   # Tidy data
   ripe.asn.lite$V1 <- as.character.factor(ripe.asn.lite$V1)
   ripe.asn.lite$V2 <- as.character.factor(ripe.asn.lite$V2)
-  names(ripe.asn.lite) <- c("aut-num","as-name","source","status")
+  names(ripe.asn.lite) <- c("aut.num","as.name","source","status")
 
   return(ripe.asn.lite)
+}
+
+#' GetRIPE.inet
+#'
+#' @param path where RIPE files will be downloaded and unziped (don't finish with /). Default set as inst/tmpdata
+#' @param download TRUE if you want to update the source data, default set as FALSE
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' ripe.inet <- GetRIPE.inet()
+#' ripe.inet <- GetRIPE.inet(download = TRUE)
+GetRIPE.inet <- function(path = "inst/tmpdata", download = FALSE, use.cached = FALSE) {
+  if (use.cached) {
+    return(readRDS(file = paste(path, "ripe/ripe.db.inetnum.rds", sep = "/")))
+  } else {
+    # Download raw data
+    path <- ifelse(download, DownloadRIPE.ASN(path), paste(path, "ripe/ripe.db.inetnum", sep = "/"))
+    ripe.inet <- read.delim(file = path, sep = ":", header = FALSE, skip = 6,
+                            quote = "\"", stringsAsFactors = FALSE)
+
+    # Raw data to filtered data frame
+    ripe.inet <- ripe.inet[ripe.inet$V1 %in% c("inetnum","netname","country","admin-c","tech-c","status","mnt-by","created","last-modified","source","org"),]
+    ripe.inet$V1 <- as.factor(ripe.inet$V1)
+    ripe.inet.lite <- ripe.inet[ripe.inet$V1 %in% c("inetnum","netname","source","status","created","last-modified"),]
+    block.mark <- 1:nrow(ripe.inet.lite) %in% grep(pattern = "inetnum", x = ripe.inet.lite$V1)
+    ripe.inet.lite <- lapply(split(ripe.inet.lite, cumsum(block.mark)), function(x) stringr::str_trim(x$V2))
+    # ripe.inet.lite size: 952 Mb aprox.
+
+    # Tidy data
+    ripe.inet.lite <- ripe.inet.lite[sapply(ripe.inet.lite, length) == 6]
+    # Next step needs improvement, it's slow
+    ripe.inet.lite <- as.data.frame.matrix(t(as.data.frame.list(ripe.inet.lite, stringsAsFactors = F)), stringsAsFactors = F)
+    names(ripe.inet.lite) <- c("inetnum","netname","status","created","last.modified","source")
+    ripe.inet.lite$source <- as.factor(ripe.inet.lite$source)
+    ripe.inet.lite$status <- as.factor(ripe.inet.lite$status)
+
+    # Save as RDS
+    saveRDS(object = ripe.inet.lite, file = paste(path, ".rds", sep = ""))
+
+    return(ripe.inet.lite)
+  }
 }
 
 #' DownloadRIPE.ASN
@@ -34,6 +78,7 @@ GetRIPE.ASN <- function(path = "inst/tmpdata", download = FALSE){
 #' @param path where files will be stored
 #' @return path of asn file unziped
 #' @examples
+#' path <- DownloadRIPE.ASN()
 DownloadRIPE.ASN <- function(path = "inst/tmpdata") {
   url <- "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.aut-num.gz"
   path <- paste(path, "ripe", sep="/")
@@ -44,39 +89,26 @@ DownloadRIPE.ASN <- function(path = "inst/tmpdata") {
   return(paste(path, "ripe.db.aut-num", sep = "/"))
 }
 
-# # Ref: https://www.ripe.net/manage-ips-and-asns/db/support/documentation/ripe-database-documentation/rpsl-object-types/4-2-descriptions-of-primary-objects/4-2-4-description-of-the-inetnum-object
-#
-# # RIPE path and local file
-# ripe.path <- "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz"
-# ripe.db.gz <- "data/ripe.db.inetnum.gz"
-# ripe.db <- "data/ripe.db.inetnum"
-#
-# # Download and unzip
-# if(!file.exists(ripe.db)){
-#     download.file(ripe.path, destfile=ripe.db.gz)
-#     gunzip(ripe.db.gz)
-# }
-#
-# # Read file as data frame (~240s)
+#' DownloadRIPE.inet
+#'
+#' @param path where files will be stored
+#' @return path of asn file unziped
+#' @examples
+#' path <- DownloadRIPE.inet()
+DownloadRIPE.inet <- function(path = "inst/tmpdata") {
+  url <- "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.inetnum.gz"
+  path <- paste(path, "ripe", sep="/")
+  dir.create(path, showWarnings = FALSE)
+  destfile <- paste(path, "ripe.db.inetnum.gz", sep = "/")
+  download.file(url, destfile)
+  R.utils::gunzip(destfile)
+  return(paste(path, "ripe.db.inetnum", sep = "/"))
+}
+
+#' REFERENCES:
+#'  - CrashCourse: http://www.slideshare.net/apnic/routing-registry-function-automation-using-rpki-rpsl
+#'  - RIPE Docs: https://www.ripe.net/manage-ips-and-asns/db/support/documentation/ripe-database-documentation/rpsl-object-types/4-2-descriptions-of-primary-objects/4-2-4-description-of-the-inetnum-object
+
 # # Parse as list of net blocks
-# # Use 6Gb and is really super-slow... it needs improvement or limit nrow when read
 # # Ref: http://www.r-bloggers.com/r-parallel-processing-using-multicore-package-2/
 # # Ref: https://encrypted.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=8&ved=0ahUKEwixwPK3l_fKAhUHnRoKHYu4BEQQFghNMAc&url=http%3A%2F%2Fwww.jstatsoft.org%2Fv40%2Fi01%2Fpaper&usg=AFQjCNH3rcSknA7oCUQvDKqs7sdmpF1gMg&sig2=7hYnVFIx80F-vBeUK6DslQ
-# ripe <- ripe2DF(10000)
-#
-# # Save ripe data frame
-# saveRDS(object = ripe, file = "data/ripe.db.rds")
-#
-#
-# ripe2DF <- function(filas = 10000){
-#     ripe <- read.delim(file = ripe.db, sep = ":", header = FALSE, skip = 6,
-#                        quote = "\"", stringsAsFactors = FALSE, nrows = filas)
-#     # Transform to data frame
-#     block.mark <- 1:nrow(ripe) %in% grep(pattern = "% Tags .*", x = ripe$V1)
-#     ripe <- split(ripe, cumsum(block.mark))
-#     selected.fields <- "^(inetnum|netname|country|org|admin-c|tech-c|status|notify|mnt-by|created|last-modified|source)$"
-#     ripe <- lapply(ripe, function(x) x[grep(pattern = selected.fields, x = x$V1, perl = T),])
-#     ripe <- lapply(ripe, function(x) as.data.frame(t(as.data.frame(x$V2, x$V1))))
-#     ripe <- do.call("rbind.fill", ripe)
-#     return(ripe)
-# }
