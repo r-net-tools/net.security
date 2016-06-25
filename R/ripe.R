f4b20a02cf804c706e88882dfe03c1a26e8044c0
Humbert <humbert.costas@gmail.com>
2016-06-25 14:44:29 +0200

    ripe.R:  - GetRIPE.ASN: returns basic information about ASN (only mandatory and single keys).
    Former-commit-id: 8e8020affdc425f9ac4b29ccf5ea09f5136a75d0

--- R/ripe.R
+++ R/ripe.R
@@ -1,3 +1,49 @@
+#' GetRIPE.ASN
+#'
+#' @param path where RIPE files will be downloaded and unziped (don't finish with /). Default set as inst/tmpdata
+#' @param download TRUE if you want to update the source data, default set as FALSE#'
+#' @return data frame
+#' @export
+#'
+#' @examples
+#' ripe.asn <- GetRIPE.ASN()
+#' ripe.asn <- GetRIPE.ASN(download = TRUE)
+GetRIPE.ASN <- function(path = "inst/tmpdata", download = FALSE){
+  #
+  path <- ifelse(download, DownloadRIPE.ASN(path), paste(path, "ripe/ripe.db.aut-num", sep = "/"))
+  ripe.asn <- read.delim(file = path, sep = ":", header = FALSE, skip = 6,
+                         quote = "\"", stringsAsFactors = FALSE)
+  ripe.asn <- ripe.asn[ripe.asn$V1 %in% c("aut-num","as-name","descr","admin-c","tech-c","mnt-by","source","org","status","created","last-modified"),]
+  ripe.asn$V1 <- as.factor(ripe.asn$V1)
+  ripe.asn.lite <- ripe.asn[ripe.asn$V1 %in% c("aut-num","as-name","source","status"),]
+  block.mark <- 1:nrow(ripe.asn.lite) %in% grep(pattern = "aut-num", x = ripe.asn.lite$V1)
+  ripe.asn.lite <- split(ripe.asn.lite, cumsum(block.mark))
+  ripe.asn.lite <- lapply(ripe.asn.lite, function(x) as.data.frame(t(as.data.frame(x$V2, x$V1))))
+  ripe.asn.lite <- do.call("rbind.fill", ripe.asn.lite)
+
+  # Tidy data
+  ripe.asn.lite$V1 <- as.character.factor(ripe.asn.lite$V1)
+  ripe.asn.lite$V2 <- as.character.factor(ripe.asn.lite$V2)
+  names(ripe.asn.lite) <- c("aut-num","as-name","source","status")
+
+  return(ripe.asn.lite)
+}
+
+#' DownloadRIPE.ASN
+#'
+#' @param path where files will be stored
+#' @return path of asn file unziped
+#' @examples
+DownloadRIPE.ASN <- function(path = "inst/tmpdata") {
+  url <- "ftp://ftp.ripe.net/ripe/dbase/split/ripe.db.aut-num.gz"
+  path <- paste(path, "ripe", sep="/")
+  dir.create(path, showWarnings = FALSE)
+  destfile <- paste(path, "ripe.db.aut-num.gz", sep = "/")
+  download.file(url, destfile)
+  R.utils::gunzip(destfile)
+  return(paste(path, "ripe.db.aut-num", sep = "/"))
+}
+
 # # Ref: https://www.ripe.net/manage-ips-and-asns/db/support/documentation/ripe-database-documentation/rpsl-object-types/4-2-descriptions-of-primary-objects/4-2-4-description-of-the-inetnum-object
 #
 # # RIPE path and local file
