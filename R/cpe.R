bd7383257d19890e935a54e57eab0d7388d0f752
Humbert <humbert.costas@gmail.com>
2016-07-09 17:18:31 +0200

    CPE:  - Added info in readme file  - GetCPEData empty function, to be done.
    Former-commit-id: 008fdc6e157ff3daeb5fd9e56ff3ecd31ccc35bb
    Former-commit-id: 777e40a56ac0d4f9bae4a264bd92b26d51a1bb76

---
+++ R/cpe.R
@@ -0,0 +1,11 @@
+#' GetCWEData
+#'
+#' @return
+#' @export
+#'
+#' @examples
+GetCPEData <- function() {
+  # Schema: https://scap.nist.gov/schema/cpe/2.3/cpe-dictionary_2.3.xsd
+  # RawData: http://static.nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.gz
+  return("")
+}
