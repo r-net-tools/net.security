aa83e0567f48dfa62e0b2f7b30d6a1ec61d51f93
Arnau Sangrà <asangra@ackcent.com>
2016-07-11 10:43:30 +0200

    Updated documentation [ci skip]
    Former-commit-id: 370e033192bd316dad6784601e316ecee04ac078
    Former-commit-id: 72d1c5c393c8c2b98ccc0c3d2f992f773a02aa2a

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Common Platform Enumeration Entries
+#'
+#' A dataset containing entries with the information of the different platforms containing
+#' over  100,000 rows.
+#'
+#' @format A data frame with +100,000 rows and 3 variables:
+#' \describe{
+#'   \item{cpe.22}{Title of the platform}
+#'   \item{cpe.23}{Identifier for given platform}
+#'   \item{cpe.ref}{URLs with additional information}
+#'   ...
+#' }
+#' @source \url{https://cpe.mitre.org}
+"cpes"
