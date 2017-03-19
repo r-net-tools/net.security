e6aa27082e1d508fdb3670184d124d7253f693d4
Humbert Costas Pujol <Humbert Costas Pujol>
2017-03-19 02:49:55 +0100

    Added some basic unit tests.
    Former-commit-id: 772628436daf3d0c8d9c5be549aa61b76d0afed1
    Former-commit-id: 0428cd80fde2dcbb7ebd1e99b12f3e4ce51bd5be
    Former-commit-id: 536d30e556a003b390b1a61eb5240de579b976a4

--- tests/testthat/test_public.R
+++ tests/testthat/test_public.R
@@ -1,2 +1,29 @@
 context("public functions")
 
+
+#### DataSetList -----------------------------------------------------------------------------------------
+x <- net.security::DataSetList()
+
+test_that("check DataSetList output",{
+  expect_true(exists("x"))
+  expect_false(is.null(x))
+  expect_is(x, "character")
+})
+
+#### DataSetStatus -----------------------------------------------------------------------------------------
+x <- net.security::DataSetStatus()
+
+test_that("check DataSetStatus output",{
+  expect_true(exists("x"))
+  expect_false(is.null(x))
+  expect_is(x, "character")
+})
+
+#### GetDataFrame -----------------------------------------------------------------------------------------
+cves <- net.security::GetDataFrame("cves")
+
+test_that("check GetDataFrame output with 'cves'",{
+  expect_true(exists("x"))
+  expect_false(is.null(x))
+  expect_is(x, "character")
+})
