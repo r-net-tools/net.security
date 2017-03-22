1b8c66d8b4204627a8b94f112489a7e15bb5f10d
Humbert Costas Pujol <Humbert Costas Pujol>
2017-03-22 09:51:12 +0100

    more changes related to cran checks
    Former-commit-id: a89a3e74fa1f9c7fcaf38f149248573abc442db1
    Former-commit-id: 6c1503b68f08ce34cc6596efd11c21a44fb2d1bb
    Former-commit-id: 40e0f62bdf23cff227301c1a6dc0803806881d4b

--- R/cve.R
+++ R/cve.R
@@ -3,10 +3,6 @@
 #' LastDownloadDate
 #'
 #' @return character with last official update date YYYY-MM-DD
-#'
-#' \dontrun{
-#' @examples last <- LastDownloadCVEDate()
-#' }
 LastDownloadCVEDate <- function(){
   doc.html <- XML::htmlParse("http://cve.mitre.org/data/downloads/index.html#download")
   txt <- XML::xmlValue(XML::xpathSApply(doc.html, '//div[@class="smaller"]')[[1]])
