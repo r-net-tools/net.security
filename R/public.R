577b3f16d767005b4c3bec9ff38a6237b278a9e9
Humbert Costas Pujol <Humbert Costas Pujol>
2017-03-22 09:29:47 +0100

    more changes related to cran checks
    Former-commit-id: 5075ef8338d9103c229168090cb94457b7dadafc
    Former-commit-id: 07d72189d4b1dcdffa7ae04eff6c54047a26663f
    Former-commit-id: 5adc8d896c7ee2a9c174c883f262027c283a0b07

--- R/public.R
+++ R/public.R
@@ -22,12 +22,11 @@ DataSetStatus <- function(ds = "all") {
     # Get Status from local cves data.frame
     if (DataSetAvailable("cves")) {
       print("-: CVES dataset:")
-      cves.timestamp <- netsec.data[[1]][["cves.ini"]]
+      cves.timestamp <- strptime(netsec.data[[1]][["cves.ini"]], format = "%Y-%m-%d")
       print(paste(" |- Last update for CVES dataset at", as.character(cves.timestamp)))
       print(paste(" |- Data set with", as.character(nrow(netsec.data[[2]][["cves"]])), "rows and",
             as.character(ncol(netsec.data[[2]][["cves"]])), "variables."))
       cveonline <- strptime(LastDownloadCVEDate(), format = "%Y-%m-%d")
-      cves.timestamp <- strptime(cves.timestamp, format = "%Y-%m-%d")
       print(paste(" |- Online RAW data updated at", cveonline))
       if ((cveonline-cves.timestamp)<=0) {
         print(paste(" |- No updates needed for CVES dataset."))
@@ -41,19 +40,18 @@ DataSetStatus <- function(ds = "all") {
     # Get Status from local cpes data.frame
     if (DataSetAvailable("cpes")) {
       print("-: CPES dataset:")
-      cpes.timestamp <- netsec.data[[1]][["cpes.ini"]]
+      cpes.timestamp <- strptime(netsec.data[[1]][["cpes.ini"]], format = "%Y-%m-%d")
       print(paste(" |- Last update for CPES dataset at", as.character(cpes.timestamp)))
       print(paste(" |- Data set with", as.character(nrow(netsec.data[[2]][["cpes"]])), "rows and",
                   as.character(ncol(netsec.data[[2]][["cpes"]])), "variables."))
       cpeonline <- strptime(LastDownloadCPEDate(), format = "%Y-%m-%d")
-      cpes.timestamp <- strptime(cpes.timestamp, format = "%Y-%m-%d")
       print(paste(" |- Online RAW data updated at", cpeonline))
       if ((cpeonline-cpes.timestamp)<=0) {
         print(paste(" |- No updates needed for CPES dataset."))
       } else {
         print(paste(" |- CPES dataset", as.character(cpeonline-cpes.timestamp), "days outdated."))
       }
-      status <- "."
+      status <- "-:"
     }
   }
   return(status)
@@ -63,7 +61,7 @@ DataSetStatus <- function(ds = "all") {
 #'
 #' @param ds Selects the data set for this operation. Default set to "all".
 #'           Check available options with DataSetList()
-#' @return Official source files download date time.
+#' @return Date Official source files download date time.
 #' @export
 #' @examples
 #' \dontrun{
@@ -86,28 +84,6 @@ DataSetUpdate <- function(ds = "all") {
     netsec.data$dwinfo[["cves.fin"]] <- Sys.Date()
     datasets[["cves"]] <- cves
     netsec.data$datasets <- datasets
-
-    # print("Checking on MITRE last update for CVEs...")
-    # print("Checking on NIST last update for CPEs...")
-    # cveonline <- strptime(LastDownloadCVEDate(), format = "%Y-%m-%d")
-    # cves.timestamp <- strptime(netsec.data[[1]][["cves.ini"]], format = "%Y-%m-%d")
-    # if ((cveonline - cves.timestamp) <= 0) {
-    #   print(paste(" |- No updates needed for CPES dataset."))
-    # } else {
-    #   print(paste(" |- CPES dataset", as.character(cpeonline-cpes.timestamp), "days outdated."))
-    # }
-
-    # upd <- LastDownloadCVEDate()
-    # if (any(grepl("cves", names(netsec.data$dwinfo))) &
-    #     (netsec.data$dwinfo$cves.ini >= upd)) {
-    #   print("You have the newest CVES dataset.")
-    # } else {
-    #
-    #   # Save sample cves data frame
-    #   # print("Compressing and saving random sample of CVEs to local file...")
-    #   # cves.sample <- cves[sample(nrow(cves), 500), ]
-    #   # save(object = cves.sample, file = "data/cves.sample.rda", compress = "xz")
-    # }
   }
 
   # Update CPES dataset
@@ -128,8 +104,9 @@ DataSetUpdate <- function(ds = "all") {
   save(object = netsec.data, file = "R/sysdata.rda", compress = "xz")
 
   # TODO: Update netsec.data in parent.env(environment())
+  # https://www.r-bloggers.com/package-wide-variablescache-in-r-packages/
   warning("Package needs rebuild to use updated data sets.")
-  # assign(x = netsec.data, value = net.sec.data, envir = parent.env(environment()))
+  # assign(x = netsec.data, value = netsec.data, envir = parent.env(environment()))
 
   return(as.character(today))
 }
