93ae69a3a53a52dd9bddfdb260fe79b8ddbd4bb4
Humbert Costas Pujol <Humbert Costas Pujol>
2017-03-26 12:07:15 +0200

    DataSetUpdate refactor. Implemented logic for 2 new parameters: samples, use.remote
    Former-commit-id: e26a55e2eb3b054d354bd7957129447addb2eca2
    Former-commit-id: c9df813ea7ef1b3e5a3e86b611a7fa5e4264bec1
    Former-commit-id: 779dd37889fc300856d13ca8c2096aa84a05b540

--- R/public.R
+++ R/public.R
@@ -73,6 +73,8 @@ DataSetStatus <- function(ds = "all") {
 #'
 #' @param ds Selects the data set for this operation. Default set to "all".
 #'           Check available options with DataSetList()
+#' @param samples if TRUE it will create sample data.frames and store them in /data
+#' @param use.remote if TRUE it will download sysdata.rda from net.security github
 #' @return Date Official source files download date time.
 #' @export
 #' @examples
@@ -82,49 +84,108 @@ DataSetStatus <- function(ds = "all") {
 #' \dontrun{
 #' net.security::DataSetUpdate(ds = "cves")
 #' }
-DataSetUpdate <- function(ds = "all") {
-  # TODO: Create netsec.data if not exists
-  # Create netsec.data structure
-  # timestamp <- list()
-  # datasets <- list()
-  # netsec.data <- list(timestamp, datasets)
-  # names(netsec.data) <- c("dwinfo","datasets")
+DataSetUpdate <- function(ds = "all", samples = FALSE, use.remote = TRUE) {
 
-  today <- Sys.Date()
-  timestamp <- list()
-  datasets <- netsec.data$datasets
+  ds <- tolower(ds)
+  if (ds %in% c("all", "cves", "cpes")) {
+    # TODO: Create netsec.data if not exists
+    # Create netsec.data structure
+    # timestamp <- list()
+    # datasets <- list()
+    # netsec.data <- list(timestamp, datasets)
+    # names(netsec.data) <- c("dwinfo","datasets")
 
-  # Update CVES dataset
-  if (tolower(ds) %in% c("cves", "all")) {
-    #  Update local cves data.frame from official sources
-    cves <- GetCVEData()
-    # Update dataset object
-    netsec.data$dwinfo[["cves.ini"]] <- today
-    netsec.data$dwinfo[["cves.fin"]] <- Sys.Date()
-    datasets[["cves"]] <- cves
+    cves.ini <- Sys.Date()
+    cpes.ini <- Sys.Date()
+    timestamp <- list()
+    datasets <- netsec.data$datasets
+    cves.nrow <- nrow(netsec.data$datasets$cves)
+    cpes.nrow <- nrow(netsec.data$datasets$cpes)
+
+    # print(paste(ls(parent.env(environment()))))
+    # netsec.data$dwinfo[["test"]] <- "OK"
+    # assign(x = "netsec.data2", value = netsec.data, envir = parent.env(environment()))
+    # netsec.data <- netsec.data2
+
+
+    # Get updated data.frames
+    if (use.remote) {
+      # Get sysdata.rda and update local data
+      download.file(url = "https://github.com/r-net-tools/net.security/raw/devel/R/sysdata.rda",
+                    destfile = paste(tempdir(),"sysdata.rda",sep = "\\"))
+      load(file = paste(tempdir(),"sysdata.rda",sep = "\\"))
+      if (tolower(ds) %in% c("cves", "all")) {
+        #  Update local cves data.frame from official sources
+        cves <- netsec.data$datasets$cves
+        cves.ini <- netsec.data$dwinfo[["cves.ini"]]
+      }
+      if (tolower(ds) %in% c("cpes", "all")) {
+        #  Update local cpes data.frame from official sources
+        cpes <- netsec.data$datasets$cpes
+        cpes.ini <- netsec.data$dwinfo[["cpes.ini"]]
+      }
+      rm(netsec.data)
+    } else {
+      if (tolower(ds) %in% c("cves", "all")) {
+        #  Update local cves data.frame from official sources
+        cves <- GetCVEData()
+      }
+      if (tolower(ds) %in% c("cpes", "all")) {
+        #  Update local cpes data.frame from official sources
+        cpes <- GetCPEData()
+      }
+    }
+
+    # Update local data
+    if (tolower(ds) %in% c("cves", "all")) {
+      #  Update package datasets with updated cves data.frame
+      netsec.data$dwinfo[["cves.ini"]] <- cves.ini
+      netsec.data$dwinfo[["cves.fin"]] <- cves.ini
+      datasets[["cves"]] <- cves
+      new.cves <- as.character(nrow(cves) - cves.nrow)
+      print(paste("-: Updated CVEs data.frame has", new.cves, " new observations."))
+    }
+    if (tolower(ds) %in% c("cpes", "all")) {
+      #  Update package datasets with updated cpes data.frame
+      netsec.data$dwinfo[["cpes.ini"]] <- cpes.ini
+      netsec.data$dwinfo[["cpes.fin"]] <- cpes.ini
+      datasets[["cpes"]] <- cpes
+      new.cpes <- as.character(nrow(cpes) - cpes.nrow)
+      print(paste("-: Updated CPEs data.frame has", new.cpes, " new observations."))
+    }
     netsec.data$datasets <- datasets
+
+    # Save samples if needed
+    if (samples) {
+      if (tolower(ds) %in% c("cves", "all")) {
+        #  Update local cves data.frame from official sources
+        # Save sample cves data frame
+        cves.sample <- cves[sample(nrow(cves), 1000), ]
+        cves.sample[] <- lapply(cves.sample, as.character)
+        save(object = cves.sample, file = "data/cves.sample.rda", compress = "xz")
+      }
+      if (tolower(ds) %in% c("cpes", "all")) {
+        #  Update local cpes data.frame from official sources
+        # Save sample cves data frame
+        cpes.sample <- cpes[sample(nrow(cpes), 1000), ]
+        cpes.sample[] <- lapply(cpes.sample, as.character)
+        # cols <- names(cves)[sapply(cves, class) == "factor"]
+        # cpes.sample[cols] <- lapply(cpes.sample[cols], factor)
+        save(object = cpes.sample, file = "data/cpes.sample.rda", compress = "xz")
+      }
+    }
+    print("Compressing and saving data sets to local file...")
+    save(object = netsec.data, file = "R/sysdata.rda", compress = "xz")
+
+    # TODO: Update netsec.data in parent.env(environment())
+    # https://www.r-bloggers.com/package-wide-variablescache-in-r-packages/
+    warning("Package needs rebuild to use updated data sets.")
+    # assign(x = netsec.data, value = netsec.data, envir = parent.env(environment()))
+  } else {
+    warning("Invalid option. Use net.security::DataSetList() to show available options.")
   }
 
-  # Update CPES dataset
-  if (tolower(ds) %in% c("cpes", "all")) {
-    #  Update local cpes data.frame from official sources
-    cpes <- GetCPEData()
-    # Update dataset object
-    netsec.data$dwinfo[["cpes.ini"]] <- today
-    netsec.data$dwinfo[["cpes.fin"]] <- Sys.Date()
-    datasets[["cpes"]] <- cpes
-    netsec.data$datasets <- datasets
-  }
-
-  print("Compressing and saving data sets to local file...")
-  save(object = netsec.data, file = "R/sysdata.rda", compress = "xz")
-
-  # TODO: Update netsec.data in parent.env(environment())
-  # https://www.r-bloggers.com/package-wide-variablescache-in-r-packages/
-  warning("Package needs rebuild to use updated data sets.")
-  # assign(x = netsec.data, value = netsec.data, envir = parent.env(environment()))
-
-  return(as.character(today))
+  return(as.character(Sys.Date()))
 }
 
 #' Show data set status. Prints information about update status and number of observations of local data sets.
