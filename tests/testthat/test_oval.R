4509caa4a11e0b40bdd5093c87527e3c81e4e99b
Humbert Costas Pujol <Humbert Costas Pujol>
2017-03-31 00:36:11 +0200

    Added some OVAL tests
    Former-commit-id: b1f4785bfcf1fd8c4c6cda786eaa2489bd8e00d1
    Former-commit-id: 4d0e02838c49c9e0be42adc12d60fe1b197c7e08
    Former-commit-id: 3ab3c44f923e0eabda3877110b85c50479708ec3

---
+++ tests/testthat/test_oval.R
@@ -0,0 +1,62 @@
+context("oval functions")
+
+#### Expected Values -------------------------------------------------------------------------------
+
+expected_oval_names    <- c("type","class","id","version","title",
+                            "affected.family","affected.platforms","affected.cpes",
+                            "references","status","deprecated")
+expected_oval_classes  <- c("factor","factor","character","factor","character",
+                            "factor","character","character","character","factor",
+                            "factor")
+
+expected_item_names <- c("class","id","version","title","affected.family",
+                         "affected.platforms","affected.cpes",
+                         "references","status")
+expected_item_classes <- c("character","character","character","character",
+                           "character","character","character","character","character")
+
+#### OVAL data fram generation ----------------------------------------------------------------------
+
+my_oval <- net.security::GetDataFrame("oval")
+
+#### TESTS -----------------------------------------------------------------------------------------
+
+test_that("oval_creation",{
+  expect_true(exists("my_oval"))
+})
+
+test_that("oval_not_null", {
+  expect_false(is.null(my_oval))
+})
+
+test_that("oval_is_dataframe", {
+  expect_is(my_oval, "data.frame")
+})
+
+test_that("oval_structure",{
+  expect_true(all.equal(expected_oval_names, names(my_oval)))
+  expect_true(all(sapply(my_oval, class) == expected_oval_classes))
+})
+
+#### Internal functions
+x <- LastDownloadOVALDate()
+
+test_that("check LastDownloadOVALDate output",{
+  expect_true(exists("x"))
+  expect_false(is.null(x))
+  expect_is(x, "character")
+})
+rm(x)
+
+x <- NewOVALItem()
+
+print(names(x))
+
+test_that("check NewOVALItem output",{
+  expect_true(exists("x"))
+  expect_false(is.null(x))
+  expect_is(x, "data.frame")
+  expect_true(all.equal(expected_item_names, names(x)))
+  expect_true(all(expected_item_classes == sapply(x, class)))
+})
+rm(x)
