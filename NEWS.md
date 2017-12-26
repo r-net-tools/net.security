8400b6dd71660c7284260323d3a90da45c0429f5
Humbert <humbert.costas@gmail.com>
2017-12-26 01:50:14 +0100

    Parser updated to CWE schema v6.0

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# net.security 0.3.1
+
+ * CWE Standard changed its schema. New parser for CWE data.frame.  
+ 
 # net.security 0.3.0
 
  * Added CAPEC Standard  
--- R/cwe.R
+++ R/cwe.R
@@ -3,29 +3,29 @@ LastDownloadCWEDate <- function(){
 }
 
 GetCWEData <- function(savepath = tempdir()) {
-  print("Downloading raw data...")
+  print("Downloading raw data from MITRE...")
   DownloadCWEData(savepath)
   print("Unzip, extract, etc...")
   cwes.file <- ExtractCWEFiles(savepath)
-  print("Processing MITRE raw data...")
+  print("Processing CWE raw data...")
   cwes <- ParseCWEData(cwes.file)
   # Sort and filter by WIP
-  cwes <- cwes[c("code_standard", "Name", "Weakness_Abstraction", "Status",
-                 "descr.summary", "descr.details", "ID", "cwe.parents",
-                 "time.intro", "consequences", "exploits", "related.capec", "ordinalities",
-                 "platforms", "aff.resources", "causal", "mitigation",  "demos",
-                 "mapping", "history", "relationship.notes", "maintenance.notes",
-                 "background", "introduction.mode", "other.notes", "functional.areas")]
-  names(cwes) <- c("code_standard", "Name", "Weakness_Abstraction", "Status",
-                   "descr.summary", "descr.details", "cwe.id", "cwe.parents.ids",
-                   "time.intro", "consequences", "exploits", "related.capec", "ordinalities",
-                   "platforms", "aff.resources", "causal", "mitigation",  "demos",
-                   "mapping", "history", "relationship.notes", "maintenance.notes",
-                   "background", "introduction.mode", "other.notes", "functional.areas")
-  cwes <- cwes[,c("code_standard", "Name", "Weakness_Abstraction", "Status",
-                  "descr.summary", "descr.details", "cwe.id", "cwe.parents.ids",
-                  "time.intro", "consequences", "related.capec", "exploits", "ordinalities",
-                  "platforms", "aff.resources", "causal", "mitigation")]
+  # cwes <- cwes[c("code_standard", "Name", "Weakness_Abstraction", "Status",
+  #                "descr.summary", "descr.details", "ID", "cwe.parents",
+  #                "time.intro", "consequences", "exploits", "related.capec", "ordinalities",
+  #                "platforms", "aff.resources", "causal", "mitigation",  "demos",
+  #                "mapping", "history", "relationship.notes", "maintenance.notes",
+  #                "background", "introduction.mode", "other.notes", "functional.areas")]
+  # names(cwes) <- c("code_standard", "Name", "Weakness_Abstraction", "Status",
+  #                  "descr.summary", "descr.details", "cwe.id", "cwe.parents.ids",
+  #                  "time.intro", "consequences", "exploits", "related.capec", "ordinalities",
+  #                  "platforms", "aff.resources", "causal", "mitigation",  "demos",
+  #                  "mapping", "history", "relationship.notes", "maintenance.notes",
+  #                  "background", "introduction.mode", "other.notes", "functional.areas")
+  # cwes <- cwes[,c("code_standard", "Name", "Weakness_Abstraction", "Status",
+  #                 "descr.summary", "descr.details", "cwe.id", "cwe.parents.ids",
+  #                 "time.intro", "consequences", "related.capec", "exploits", "ordinalities",
+  #                 "platforms", "aff.resources", "causal", "mitigation")]
   print(paste("CWES data frame building process finished."))
   return(cwes)
 }
@@ -49,8 +49,11 @@ ExtractCWEFiles <- function(savepath) {
 }
 
 ParseCWEData <- function(cwes.file) {
+
+  print("Parsing basic attributes...")
+
   # Load Weakness raw data
-  doc <- rvest::html(cwes.file)
+  doc <- suppressWarnings(rvest::html(cwes.file))
   raw.cwes <- rvest::html_nodes(doc, "weakness")
   # Extract Weakness node attributes
   cwes <- as.data.frame(t(sapply(raw.cwes, rvest::html_attrs)), stringsAsFactors = F)
@@ -62,31 +65,42 @@ ParseCWEData <- function(cwes.file) {
   # Add extra field with code standard
   cwes$Code_Standard <- paste("CWE-", cwes$ID, sep = "")
 
+  print("Parsing Description...")
+
   cwes$Description <- sapply(rvest::html_nodes(doc, xpath = "//weakness/description"),
                              rvest::html_text)
   cwes$Extended_Description <- sapply(raw.cwes,
                                       function(x) {ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "extended_description")), character(0)),
                                                           yes = "",
                                                           no = rvest::html_text(rvest::html_nodes(x, "extended_description")))})
+
+  print("Parsing Related Weakness...")
+
   cwes$Related_Weakness <- sapply(raw.cwes,
                                   function(x) {ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "related_weakness")), character(0)),
-                                                      yes = RJSONIO::toJSON(""),
+                                                      yes = "[\"\"]",
                                                       no = RJSONIO::toJSON(lapply(rvest::html_nodes(x, "related_weakness"),
                                                                                   rvest::html_attrs),
                                                                             pretty = T))
                                               }
                                   )
+
+  print("Parsing Weakness Ordinality...")
+
   cwes$Weakness_Ordinality <- sapply(raw.cwes,
                                     function(x) {ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "weakness_ordinality")), character(0)),
-                                                        yes = RJSONIO::toJSON(""),
+                                                        yes = "[\"\"]",
                                                         no = RJSONIO::toJSON(lapply(rvest::html_nodes(x, "weakness_ordinality"),
                                                                                      function(x) rvest::html_text(rvest::html_children(x))),
                                                                               pretty = T))
                                     }
   )
+
+  print("Parsing Applicable Platforms...")
+
   cwes$Applicable_Platforms <- sapply(raw.cwes,
                                   function(x) {ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "applicable_platforms")), character(0)),
-                                                      yes = RJSONIO::toJSON(""),
+                                                      yes = "{}",
                                                       no = {
                                                              y <- lapply(rvest::html_children(rvest::html_nodes(x, "applicable_platforms")), rvest::html_attrs)
                                                              names(y) <- rvest::html_name(rvest::html_children(rvest::html_nodes(x, "applicable_platforms")))
@@ -94,23 +108,32 @@ ParseCWEData <- function(cwes.file) {
                                                       })
                                   }
   )
+
+  print("Parsing Background Details...")
+
   cwes$Background_Details <- sapply(raw.cwes,
                                   function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "background_details")), character(0)),
-                                                     yes = RJSONIO::toJSON(""),
+                                                     yes = "[\"\"]",
                                                      no = RJSONIO::toJSON(lapply(rvest::html_nodes(x, "background_details"),
                                                                                  rvest::html_text),
                                                                           pretty = T))
   )
+
+  print("Parsing Alternate Terms...")
+
   cwes$Alternate_Terms <- sapply(raw.cwes,
                                     function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "alternate_terms")), character(0)),
-                                                       yes = RJSONIO::toJSON(""),
+                                                       yes = "[\"\"]",
                                                        no = RJSONIO::toJSON(lapply(rvest::html_children(rvest::html_nodes(x, "alternate_terms")),
                                                                                    rvest::html_text),
                                                                             pretty = T))
   )
+
+  print("Parsing Modes Of Introduction...")
+
   cwes$Modes_Of_Introduction <- sapply(raw.cwes,
                                  function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "modes_of_introduction")), character(0)),
-                                                    yes = RJSONIO::toJSON(""),
+                                                    yes = "[\"\"]",
                                                     no = RJSONIO::toJSON(lapply(
                                                                                   lapply(rvest::html_children(rvest::html_nodes(x, "modes_of_introduction")),
                                                                                          function(x) rvest::html_children(x)),
@@ -122,15 +145,22 @@ ParseCWEData <- function(cwes.file) {
                                                                                 ),
                                                     pretty = T))
   )
+
+  print("Parsing Likelihood Of Exploit...")
+
   cwes$Likelihood_Of_Exploit <- sapply(raw.cwes,
                                  function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "likelihood_of_exploit")), character(0)),
                                                     yes = "",
                                                     no = rvest::html_text(rvest::html_nodes(x, "likelihood_of_exploit"))
                                                     )
   )
+  cwes$Likelihood_Of_Exploit <- as.factor(cwes$Likelihood_Of_Exploit)
+
+  print("Parsing Common Consequences...")
+
   cwes$Common_Consequences <- sapply(raw.cwes,
                                        function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "common_consequences")), character(0)),
-                                                          yes = RJSONIO::toJSON(""),
+                                                          yes = "[\"\"]",
                                                           no = RJSONIO::toJSON(lapply(
                                                             lapply(rvest::html_children(rvest::html_nodes(x, "common_consequences")),
                                                                    function(x) rvest::html_children(x)),
@@ -142,9 +172,12 @@ ParseCWEData <- function(cwes.file) {
                                                           ),
                                                           pretty = T))
   )
+
+  print("Parsing Detection Methods...")
+
   cwes$Detection_Methods <- sapply(raw.cwes,
                                      function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "detection_methods")), character(0)),
-                                                        yes = RJSONIO::toJSON(""),
+                                                        yes = "[\"\"]",
                                                         no = RJSONIO::toJSON(lapply(
                                                           lapply(rvest::html_children(rvest::html_nodes(x, "detection_methods")),
                                                                  function(x) rvest::html_children(x)),
@@ -156,9 +189,12 @@ ParseCWEData <- function(cwes.file) {
                                                         ),
                                                         pretty = T))
   )
+
+  print("Parsing Potential Mitigations...")
+
   cwes$Potential_Mitigations <- sapply(raw.cwes,
                                    function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "potential_mitigations")), character(0)),
-                                                      yes = RJSONIO::toJSON(""),
+                                                      yes = "[\"\"]",
                                                       no = RJSONIO::toJSON(lapply(
                                                         lapply(rvest::html_children(rvest::html_nodes(x, "potential_mitigations")),
                                                                function(x) rvest::html_children(x)),
@@ -170,9 +206,12 @@ ParseCWEData <- function(cwes.file) {
                                                       ),
                                                       pretty = T))
   )
+
+  print("Parsing Observed Examples...")
+
   cwes$Observed_Examples <- sapply(raw.cwes,
                                        function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "observed_examples")), character(0)),
-                                                          yes = RJSONIO::toJSON(""),
+                                                          yes = "[\"\"]",
                                                           no = RJSONIO::toJSON(lapply(
                                                             lapply(rvest::html_children(rvest::html_nodes(x, "observed_examples")),
                                                                    function(x) rvest::html_children(x)),
@@ -184,19 +223,28 @@ ParseCWEData <- function(cwes.file) {
                                                           ),
                                                           pretty = T))
   )
+
+  print("Parsing Functional Areas...")
+
   cwes$Functional_Areas <- sapply(raw.cwes,
                                    function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "functional_areas")), character(0)),
-                                                      yes = RJSONIO::toJSON(""),
+                                                      yes = "[\"\"]",
                                                       no = RJSONIO::toJSON(sapply(rvest::html_children(rvest::html_nodes(x, "functional_areas")), rvest::html_text)))
   )
+
+  print("Parsing Affected Resources...")
+
   cwes$Affected_Resources <- sapply(raw.cwes,
                                   function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "affected_resources")), character(0)),
-                                                     yes = RJSONIO::toJSON(""),
+                                                     yes = "[\"\"]",
                                                      no = RJSONIO::toJSON(sapply(rvest::html_children(rvest::html_nodes(x, "affected_resources")), rvest::html_text)))
   )
+
+  print("Parsing Taxonomy Mappings...")
+
   cwes$Taxonomy_Mappings <- sapply(raw.cwes,
                                    function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "taxonomy_mappings")), character(0)),
-                                                      yes = RJSONIO::toJSON(""),
+                                                      yes = "[\"\"]",
                                                       no = RJSONIO::toJSON({w <- lapply(
                                                                                         lapply(rvest::html_children(rvest::html_nodes(x, "taxonomy_mappings")),
                                                                                                function(x) rvest::html_children(x)),
@@ -208,9 +256,12 @@ ParseCWEData <- function(cwes.file) {
                                                                             names(w) <- unlist(rvest::html_attrs(rvest::html_children(rvest::html_nodes(x, "taxonomy_mappings"))))
                                                                             w}, pretty = T))
   )
+
+  print("Parsing Related Attack Patterns...")
+
   cwes$Related_Attack_Patterns <- sapply(raw.cwes,
                                     function(x) ifelse(test = identical(rvest::html_text(rvest::html_nodes(x, "related_attack_patterns")), character(0)),
-                                                       yes = RJSONIO::toJSON(""),
+                                                       yes = "[\"\"]",
                                                        no = RJSONIO::toJSON(sapply(rvest::html_children(rvest::html_nodes(x, "related_attack_patterns")), rvest::html_attrs)))
   )
 
