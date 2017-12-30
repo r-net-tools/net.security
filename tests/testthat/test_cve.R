6fb0778a5c95bc2c2a6389104a29326d1a698cb9
Humbert <humbert.costas@gmail.com>
2017-12-30 22:34:04 +0100

    Update cve tests.

--- tests/testthat/test_cve.R
+++ tests/testthat/test_cve.R
@@ -2,13 +2,25 @@ context("cve functions")
 
 #### Expected Values -------------------------------------------------------------------------------
 
-expected_cves_names    <- c("cve", "status", "description", "ref.mitre", "phase",
-                            "votes", "comments", "cpe.config", "cpe.software",
-                            "discovered.datetime", "disclosure.datetime", "exploit.publish.datetime",
-                            "published.datetime", "last.modified.datetime", "cvss", "cvss.vector",
-                            "security.protection", "assessment.check", "cwe", "ref.nist", "fix.action",
-                            "scanner", "summary", "technical.description", "attack.scenario")
-expected_cves_classes  <- c("character", "factor", "character", "character", "character", "character", "character")
+expected_cves_names    <- c("cve.id", "affects", "problem.type", "references",
+                            "description", "vulnerable.configuration", "cvss3.vector",
+                            "cvss3.av", "cvss3.ac", "cvss3.pr", "cvss3.ui", "cvss3.s",
+                            "cvss3.c", "cvss3.i", "cvss3.a", "cvss3.score",
+                            "cvss3.severity", "cvss3.score.exploit", "cvss3.score.impact",
+                            "cvss2.vector", "cvss2.av", "cvss2.ac", "cvss2.au",
+                            "cvss2.c", "cvss2.i", "cvss2.a", "cvss2.score",
+                            "cvss2.score.exploit", "cvss2.score.impact",
+                            "cvss2.getallprivilege", "cvss2.getusrprivilege",
+                            "cvss2.getothprivilege", "cvss2.requsrinter",
+                            "published.date", "last.modified")
+expected_cves_classes  <- c("character", "character", "character", "character",
+                            "character", "character", "character", "factor",
+                            "factor", "factor", "factor", "factor", "factor",
+                            "factor", "factor", "numeric", "factor", "numeric",
+                            "numeric", "character", "factor", "factor", "factor",
+                            "factor", "factor", "factor", "numeric", "numeric",
+                            "numeric", "logical", "logical", "logical", "logical",
+                            "POSIXlt", "POSIXlt")
 
 
 #### CVE data fram generation ----------------------------------------------------------------------
@@ -32,14 +44,14 @@ test_that("cves_is_dataframe", {
 test_that("cves_structure",{
   expect_true(all.equal(expected_cves_names, names(my_cves)))
 
-  # actual.classes  <- sapply(my_cves, class)
-  # actual.classes  <- sapply(actual.classes, `[[`, 1) # time columns have two clases...
-  # expect_true(all(mapply(function(x,y) {return(x == y)}, actual.classes, expected_cves_classes )))
+  actual.classes  <- sapply(my_cves, class)
+  actual.classes  <- sapply(actual.classes, `[[`, 1) # time columns have two clases...
+  expect_true(all(mapply(function(x,y) {return(x == y)}, actual.classes, expected_cves_classes )))
 })
 
 test_that("cves_content", {
   cve_pattern <- "CVE-[[:digit:]]"
-  expect_true(all(grepl(pattern = cve_pattern, x = my_cves$cve)))
+  expect_true(all(grepl(pattern = cve_pattern, x = my_cves$cve.id)))
 })
 
 #### Internal functions
