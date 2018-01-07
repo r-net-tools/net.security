82f447ba8fc5e4d816cd589539ea5444ed7c1b4b
Humbert <humbert.costas@gmail.com>
2018-01-08 00:24:57 +0100

    Update sample cpe data.frame documentation.

--- data/cpes.sample.R
+++ data/cpes.sample.R
@@ -3,9 +3,9 @@
 #' A data set containing public information about CPE information from NIST.
 #'
 #' \describe{
-#'   \item{cpe.23}{CPE v2.3 ID}
-#'   \item{cpe.ref}{References as json}
-#'   \item{title}{CPE Human readable name}
+#'   \item{title}{Human-readable title of the name. To support uses intended for multiple languages, the <cpe_dict:title> element supports the xml:lang attribute. At most one <cpe_dict:title> element MAY appear for each language.}
+#'   \item{cpe.22}{The identifier name bound in CPE 2.2 format.}
+#'   \item{cpe.23}{The identifier name bound in CPE 2.3 format.}
 #'   \item{part}{The part attribute SHALL have one of these three string values:
 #'                   - The value 'a', when the CPE is for a class of applications.
 #'                   - The value 'o', when the WFN is for a class of operating systems.
@@ -19,7 +19,10 @@
 #'   \item{language}{Values for this attribute SHALL be valid language tags as defined by [RFC5646], and SHOULD be used to define the language supported in the user interface of the product being described.}
 #'   \item{sw_edition}{Values for this attribute SHOULD characterize how the product is tailored to a particular market or class of end users.}
 #'   \item{target_sw}{Values for this attribute SHOULD characterize the software computing environment within which the product operates.}
+#'   \item{target_hw}{Values for this attribute SHOULD characterize the software computing environment within which the product operates.}
 #'   \item{other}{Values for this attribute SHOULD capture any other general descriptive or identifying information which is vendor- or product-specific and which does not logically fit in any other attribute value.}
+#'   \item{references}{External references to additional descriptive material. Each reference consists of a piece of text (intended to be human-readable) and a URI (intended to be a URL pointing to a real resource).}
+#'   \item{checks}{Calls out a check, such as an OVAL definition, that can confirm or reject an IT system as an instance of the named platform. Includes a REQUIRED check system specification URI (e.g., the URI for a particular version of OVAL), a REQUIRED check identifier, and an OPTIONAL external file reference (for example, a pointer to the file where the check is defined).}
 #' }
 #'
 #' @docType data
