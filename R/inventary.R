523021cdcec657481aa173bf567ea8394cfe6179
Humbert <humbert.costas@gmail.com>
2018-01-09 02:05:33 +0100

    PoC GetInventary with Windows.

--- R/inventary.R
+++ R/inventary.R
@@ -1,14 +1,104 @@
-SoftwareCheck <- function(){
-  return("")
-}
-
-GetWindowsSoftware <- function(savepath = tempdir()){
+#' Return data.frame with installed software name, version and vendor.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+GetInventary <- function(){
   if (.Platform$OS.type == "windows") {
     # Windows with powershell
-    system("powershell.exe \"Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
-    system("powershell.exe \"Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
-    system("powershell.exe \"Get-WmiObject Win32_Product | Sort-Object Name | Format-List Name, Version, Vendor\"", intern = T)
+    sw1 <- system("powershell.exe \"Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
+    sw2 <- system("powershell.exe \"Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
+    sw3 <- system("powershell.exe \"Get-WmiObject Win32_Product | Sort-Object Name | Format-List Name, Version, Vendor\"", intern = T)
+
+    NewSWEntry <- function(name = "", version = "", vendor = ""){return(data.frame(name = name, version = version, vendor = vendor, stringsAsFactors = F))}
+
+    # SW1
+    df.sw1 <- NewSWEntry()
+    for (linia in sw1) {
+      new.sw <- NewSWEntry()
+      new.row <- lapply(stringr::str_split(linia, ":"), stringr::str_trim)[[1]]
+      switch(new.row[1],
+        "DisplayName" = {
+          new.sw.name <- new.row[2]
+          },
+        "DisplayVersion" = {
+          new.sw.version <- new.row[2]
+        },
+        "Publisher" = {
+          new.sw.publisher <- new.row[2]
+        },
+        "InstallDate" = {
+          new.sw <- NewSWEntry(new.sw.name, new.sw.version, new.sw.publisher)
+          new.sw.name <- new.sw.version <- new.sw.publisher <- ""
+          df.sw1 <- dplyr::bind_rows(df.sw1, new.sw)
+        },
+        {
+          # Default
+        }
+      )
+    }
+    df.sw1 <- df.sw1[!apply(df.sw1 == "", 1, all),]
+
+    # SW2
+    df.sw2 <- NewSWEntry()
+    for (linia in sw2) {
+      new.sw <- NewSWEntry()
+      new.row <- lapply(stringr::str_split(linia, ":"), stringr::str_trim)[[1]]
+      switch(new.row[1],
+             "DisplayName" = {
+               new.sw.name <- new.row[2]
+             },
+             "DisplayVersion" = {
+               new.sw.version <- new.row[2]
+             },
+             "Publisher" = {
+               new.sw.publisher <- new.row[2]
+             },
+             "InstallDate" = {
+               new.sw <- NewSWEntry(new.sw.name, new.sw.version, new.sw.publisher)
+               new.sw.name <- new.sw.version <- new.sw.publisher <- ""
+               df.sw2 <- dplyr::bind_rows(df.sw2, new.sw)
+             },
+             {
+               # Default
+             }
+      )
+    }
+    df.sw2 <- df.sw2[!apply(df.sw2 == "", 1, all),]
+
+    # SW3
+    df.sw3 <- NewSWEntry()
+    for (linia in sw3) {
+      new.sw <- NewSWEntry()
+      new.row <- lapply(stringr::str_split(linia, ":"), stringr::str_trim)[[1]]
+      switch(new.row[1],
+             "Name" = {
+               new.sw.name <- new.row[2]
+             },
+             "Version" = {
+               new.sw.version <- new.row[2]
+             },
+             "Vendor" = {
+               new.sw.publisher <- new.row[2]
+               new.sw <- NewSWEntry(new.sw.name, new.sw.version, new.sw.publisher)
+               new.sw.name <- new.sw.version <- new.sw.publisher <- ""
+               df.sw3 <- dplyr::bind_rows(df.sw3, new.sw)
+             },
+             {
+               # Default
+             }
+      )
+    }
+    df.sw3 <- df.sw3[!apply(df.sw3 == "", 1, all),]
+
+    df.sw <- dplyr::bind_rows(df.sw1, df.sw2, df.sw3)
+    df.sw <- df.sw[!duplicated(df.sw),]
+    df.sw <- dplyr::arrange(df.sw, name)
+
+    return(df.sw)
+  } else {
+    print("Only tested on Windows 10. Sorry.")
+    return(NA)
   }
-
-
 }
