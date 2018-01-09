467a9aa907a9821aaacc93954d662093f69eed5c
Humbert <humbert.costas@gmail.com>
2018-01-09 01:16:19 +0100

    PoC on Windows using powershell commands.

--- R/inventary.R
+++ R/inventary.R
@@ -2,9 +2,13 @@ SoftwareCheck <- function(){
   return("")
 }
 
-GetWindowsSoftware <- function(){
-  # Windows with powershell
-  # Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > .\ps.softwarelist.csv
-  # Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize > .\ps.softwarelist2.csv
-  # Get-WmiObject Win32_Product | Sort-Object Name | Format-Table Name, Version, Vendor -AutoSize > .\ps.softwarelist3.csv
+GetWindowsSoftware <- function(savepath = tempdir()){
+  if (.Platform$OS.type == "windows") {
+    # Windows with powershell
+    system("powershell.exe \"Get-ItemProperty HKLM:\\Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
+    system("powershell.exe \"Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-List\"", intern = T)
+    system("powershell.exe \"Get-WmiObject Win32_Product | Sort-Object Name | Format-List Name, Version, Vendor\"", intern = T)
+  }
+
+
 }
