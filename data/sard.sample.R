d0cdba3aa82fc143eceb719503c9e7e17225ccb5
Humbert <humbert.costas@gmail.com>
2018-01-23 22:14:47 +0100

    Minimal description for SARD sample data set.

---
+++ data/sard.sample.R
@@ -0,0 +1,19 @@
+#' Sample of 1000 random SARD
+#'
+#' A data set containing public information about SARD information from NIST.
+#'
+#' \describe{
+#' }
+#'
+#' @docType data
+#'
+#' @name sard.sample
+#'
+#' @usage data(sard.sample)
+#'
+#' @format A data frame with 100 rows and 40 columns.
+#'
+#' @keywords sard
+#'
+#' @source \url{https://samate.nist.gov/SRD/}
+"sard.sample"
