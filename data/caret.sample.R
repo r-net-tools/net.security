3f0fa38b8b02f88e7fa24453d023f23092797b66
Humbert <humbert.costas@gmail.com>
2018-07-14 04:35:59 +0200

    Update samples, Minimal documentation.

---
+++ data/caret.sample.R
@@ -0,0 +1,20 @@
+#' Sample random CARET
+#'
+#' A data set containing public information about CARET information from MITRE.
+#'
+#' \describe{
+#'   \item{list of 5 data sets}{}
+#' }
+#'
+#' @docType data
+#'
+#' @name caret.sample
+#'
+#' @usage data(caret.sample)
+#'
+#' @format A list of 5 data frames.
+#'
+#' @keywords caret
+#'
+#' @source \url{https://car.mitre.org/caret/}
+"caret.sample"
