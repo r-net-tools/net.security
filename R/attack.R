1c8a6757b6f97f432f6f538fa82318526a1ff3ad
Humbert <humbert.costas@gmail.com>
2018-07-30 22:58:33 +0200

    WIP: ParseTactic function

--- R/attack.R
+++ R/attack.R
@@ -300,12 +300,53 @@ ParseMatrix <- function() {
 }
 
 ParseTactics <- function(tactic.urls) {
-  src.url <- tactic.urls[1]
-  doc <- xml2::read_html(src.url)
+  ExtractTactic <- function(src.url) {
+    doc <- xml2::read_html(src.url)
 
-  tactics <- data.frame()
+    # Parse headers as list of nodes
+    headlines <- rvest::html_nodes(x = doc, xpath = '//*[self::h2]')
+    xpath <- sprintf("//p[count(preceding-sibling::h2)=%d]", seq_along(headlines) - 1)
 
-  return(tactics)
+    df <- purrr::set_names(purrr::map_chr(purrr::map(purrr::map(xpath, ~rvest::html_nodes(x = doc, xpath = .x)),
+                                                     rvest::html_text, trim = TRUE),
+                                          paste, collapse = "\n")
+                           ,
+                           rvest::html_text(rvest::html_node(headlines, "span")))
+    df <- as.data.frame(t(df[df != ""]), stringsAsFactors = FALSE)
+
+    t <- data.frame(tactic = stringr::str_split(src.url, "/")[[1]][length(stringr::str_split(src.url, "/")[[1]])],
+                    tactic.descr = df[,1],
+                    stringsAsFactors = FALSE)
+
+    # Extract techniques description
+    tact <- rvest::html_nodes(x = doc, xpath = "//div/table/tr")
+
+    t.tech.title <- sapply(tact, function(x) rvest::html_text(rvest::html_nodes(x, xpath = "./td/a/@title")))
+    t.tech.title <- as.character(sapply(t.tech.title, function(x) stringr::str_split(x, "/")[[1]][length(stringr::str_split(x, "/")[[1]])]))
+    t.tech.name <- sapply(tact, function(x) rvest::html_text(rvest::html_nodes(x, xpath = "./td[1]")))
+    t.tech.descr <- sapply(tact, function(x) rvest::html_text(rvest::html_nodes(x, xpath = "./td[3]")))
+
+    t.tech <- data.frame(tactic = rep(t$tactic, length(t.tech.name)),
+                         tactic.descr = rep(t$tactic.descr, length(t.tech.name)),
+                         technique = t.tech.title,
+                         technique.name = t.tech.name,
+                         technique.desc = t.tech.descr,
+                         stringsAsFactors = FALSE)
+
+    return(t.tech)
+  }
+
+  df <- data.frame(tactic = character(),
+                   tactic.descr = character(),
+                   technique = character(),
+                   technique.name = character(),
+                   technique.desc = character(),
+                   stringsAsFactors = FALSE)
+  for (src.url in tactic.urls) {
+    df <- rbind(df, ExtractTactic(src.url))
+  }
+
+  return(df)
 }
 
 ParseTechniques <- function() {
