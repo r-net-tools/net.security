c076a5c7565df5bfc88d6d4a3827deda0e4fc5d5
Humbert Costas <hcostas@ackcent.com>
2018-08-31 03:17:43 +0200

    ParseCARDataModel done. ParseCARData done.

--- R/caret.R
+++ R/caret.R
@@ -2,7 +2,7 @@ GetCARETData <- function(savepath = tempdir(), verbose = T) {
   if (verbose) print("Downloading raw data from MITRE...")
   # caret.file <- DownloadCARETData(savepath)
   if (verbose) print("Processing CARET raw data...")
-  car <- ParseCARETData(verbose)
+  car <- ParseCAR(verbose)
   if (verbose) print(paste("CARET data frame building process finished."))
   return(caret)
 }
@@ -18,8 +18,106 @@ DownloadCARETData <- function(savepath) {
   return(caret.file)
 }
 
-ParseCARETData <- function(verbose) {
-  if (verbose) print("[CAR]  - Processing Cyber Analytic Repository basic information...")
+ParseCAR <- function(verbose = TRUE) {
+  if (verbose) print("Processing CAR raw data...")
+  if (verbose) print("[+] Cyber Analytic Repository Data Model")
+  datamodel.raw <- ParseCARDataModel(verbose)
+  if (verbose) print("[+] Cyber Analytic Repository Sensors")
+  sensors.raw <- ParseCARSensors(verbose)
+  if (verbose) print("[+] Cyber Analytic Repository information")
+  car.raw <- ParseCARData(verbose)
+
+  relations <- ParseCARRelations(datamodel.raw, sensors.raw, car.raw, verbose)
+}
+
+ParseCARDataModel <- function(verbose) {
+  if (verbose) print("[CAR]  - Processing Data Models raw data...")
+  source.url <- "https://car.mitre.org/wiki/Category:Data_Model"
+  doc <- xml2::read_html(RCurl::getURL(url = source.url, ssl.verifypeer = FALSE))
+
+  dm.sources <- rvest::html_text(rvest::html_nodes(x = doc, xpath = '//*[@id="mw-pages"]/div/div/div/ul/li/a/@href'))
+  dm.ids <- stringr::str_replace(dm.sources, "/wiki/Data_Model/", "")
+  dm.sources <- as.character(sapply(dm.sources, function(x) paste("https://car.mitre.org", x, sep = "")))
+
+  df.basic <- data.frame(id = dm.ids,
+                         source = dm.sources,
+                         stringsAsFactors = FALSE)
+
+  if (verbose) print("[CAR]  - Processing Data Model details and relationships...")
+  df <- data.frame(id = character(),
+                   description = character(),
+                   action = character(),
+                   field = character(),
+                   stringsAsFactors = FALSE)
+
+  if (verbose) {pb <- utils::txtProgressBar(min = 0, max = nrow(df.basic), style = 3); i <- 1}
+
+  for (src.url in unique(df.basic$source)) {
+    df <- rbind(df, ExtractCARDataModel(src.url))
+    if (verbose) {utils::setTxtProgressBar(pb, i); i <- i + 1}
+  }
+
+  df <- dplyr::left_join(df.basic, df, by = c("id"))
+
+  return(df)
+}
+
+ExtractCARDataModel <- function(source.url = "https://car.mitre.org/wiki/Data_Model/driver") {
+  source.html <- RCurl::getURL(url = source.url, ssl.verifypeer = FALSE)
+  source.html <- stringr::str_replace_all(source.html, "<br />", ", ")
+  doc <- xml2::read_html(source.html)
+  dm.id <- stringr::str_replace(source.url, "https://car.mitre.org/wiki/Data_Model/", "")
+
+  # Parse headers as list of nodes
+  headlines <- rvest::html_nodes(x = doc, xpath = '//*[self::h2]')
+  xpath <- sprintf("//p[count(preceding-sibling::h2)=%d]", seq_along(headlines) - 1)
+
+  dm <- purrr::set_names(purrr::map_chr(purrr::map(purrr::map(xpath, ~rvest::html_nodes(x = doc, xpath = .x)),
+                                                   rvest::html_text, trim = TRUE),
+                                        paste, collapse = "\n")
+                         ,
+                         rvest::html_text(rvest::html_node(headlines, "span")))
+  dm <- as.data.frame(t(dm[dm != ""]), stringsAsFactors = FALSE)
+
+  dm <- data.frame(id = stringr::str_replace(source.url, "https://car.mitre.org/wiki/Data_Model/", ""),
+                   description = dm[,1],
+                   stringsAsFactors = FALSE)
+
+  # Extract related techniques
+  fields <- rvest::html_text(rvest::html_nodes(x = doc, xpath = '//*[@id="mw-content-text"]/table/tr[1]/th'))
+  fields <- fields[fields != ""]
+
+  actions <- rvest::html_text(rvest::html_nodes(x = doc, xpath = '//*[@id="mw-content-text"]/table/tr[position()>1]/th'))
+
+  df <- as.data.frame.matrix(merge(actions, fields))
+  names(df) <- c("action", "field")
+  df$id <- rep(dm.id, nrow(df))
+
+  # Extract Sensors relationship
+  cover.map <- rvest::html_table(xml2::xml_find_first(doc, xpath = '//table[@class="sensor_table wikitable"]'))
+  rn.cm <- cover.map[,1]
+  cover.map <- cover.map[,-1]
+  cover.map <- as.data.frame.matrix(apply(cover.map, 2, function(x) stringr::str_replace(x, ",$", "")), stringsAsFactors = FALSE)
+  row.names(cover.map) <- rn.cm
+
+  df <- dplyr::left_join(dm, df, by = c("id"))
+  df$sensor <- apply(df, 1, function(x) cover.map[x[3],x[4]])
+
+  df <- tidyr::separate_rows(df, sensor, sep = ", ")
+  df$sensor[which(df$sensor == "")] <- NA
+
+  return(df)
+}
+
+ParseCARSensors <- function(verbose) {
+  if (verbose) print("[CAR]  - Processing Sensors raw data...")
+  source.url <- "https://car.mitre.org/wiki/Category:Sensors"
+  doc <- xml2::read_html(RCurl::getURL(url = source.url, ssl.verifypeer = FALSE))
+
+}
+
+ParseCARData <- function(verbose) {
+  if (verbose) print("[CAR]  - Processing CAR basic information...")
 
   source.url <- "https://car.mitre.org/wiki/Full_Analytic_List"
   doc <- xml2::read_html(RCurl::getURL(url = source.url, ssl.verifypeer = FALSE))
@@ -43,7 +141,7 @@ ParseCARETData <- function(verbose) {
                          source = car.url,
                          stringsAsFactors = FALSE)
 
-  if (verbose) print("[CAR]  - Processing Cyber Analytic Repository details and relationships...")
+  if (verbose) print("[CAR]  - Processing CAR details and relationships...")
   df <- data.frame(id = character(),
                    type = character(),
                    stringsAsFactors = FALSE)
@@ -81,6 +179,9 @@ ExtractCAR <- function(source.url = "https://car.mitre.org/wiki/CAR-2013-05-009"
   if ("information.domain" %in% names(df)) {
     df <- tidyr::separate_rows(df, information.domain, sep = ", ")
   }
+  if ("host.subtypes" %in% names(df)) {
+    df <- tidyr::separate_rows(df, host.subtypes, sep = ", ")
+  }
   if ("network.subtypes" %in% names(df)) {
     df <- tidyr::separate_rows(df, network.subtypes, sep = ", ")
   }
@@ -109,137 +210,51 @@ ExtractCAR <- function(source.url = "https://car.mitre.org/wiki/CAR-2013-05-009"
   return(df)
 }
 
+ParseCARRelations <- function(datamodel.raw, sensors.raw, car.raw, verbose) {
+  df <- data.frame(from = character(),
+                   to = character(),
+                   source = character(),
+                   target = character(),
+                   info = character(),
+                   stringsAsFactors = FALSE)
 
-# ParseCARETData2 <- function(caret.file, verbose) {
-#   caret.raw <- RJSONIO::fromJSON(caret.file)
-#
-#   car.data.model <- plyr::ldply(caret.raw$dataModel,
-#                                 function(x) {
-#                                   plyr::ldply(x$actions,
-#                                               function(y) {
-#                                                 data.frame(object = rep(x$name, length(x$fields)),
-#                                                            action = rep(y, length(x$fields)),
-#                                                            field = x$fields,
-#                                                            stringsAsFactors = F)
-#                                               }
-#                                   )
-#                                 })
-#   car.sensors <- plyr::ldply(caret.raw$sensors,
-#                              function(x) {
-#                                data.frame(name = rep(x$name, length(x$fields)),
-#                                           field = x$fields,
-#                                           stringsAsFactors = F)
-#                              })
-#   car.groups <- plyr::ldply(caret.raw$groups,
-#                             function(x) {
-#                               plyr::ldply(x$techniques,
-#                                           function(y) {
-#                                             data.frame(name = x$name,
-#                                                        technique = y,
-#                                                        id = x$ID,
-#                                                        aliases = as.character(jsonlite::toJSON(x$aliases)),
-#                                                        stringsAsFactors = F)
-#                                           }
-#                               )
-#                             })
-#   car.techniques <- plyr::ldply(caret.raw$techniques,
-#                                 function(x) {
-#                                   plyr::ldply(x$tactics,
-#                                               function(y) {
-#                                                 data.frame(url = paste("https://attack.mitre.org/wiki", x$name, sep = "/"),
-#                                                            tactic = y,
-#                                                            id = x$ID,
-#                                                            display_name = x$display_name,
-#                                                            stringsAsFactors = F)
-#                                               }
-#                                   )
-#                                 })
-#
-#   car.analytics <- plyr::ldply(caret.raw$analytics,
-#                                function(x) {
-#                                  plyr::ldply(x[[2]],
-#                                              function(y) {
-#                                                if (length(y$tactics) > 1) {
-#                                                  plyr::ldply(y$tactics, function(z) {
-#                                                    data.frame(id = x$name,
-#                                                               name = x$shortName,
-#                                                               tactic.name = z,
-#                                                               cover = y$coverage,
-#                                                               tech.id = stringr::str_replace(string = y$technique, pattern = "Technique/", replacement = ""),
-#                                                               stringsAsFactors = F)
-#                                                  })
-#                                                } else {
-#                                                  data.frame(id = x$name,
-#                                                             name = x$shortName,
-#                                                             tactic.name = y$tactics,
-#                                                             cover = y$coverage,
-#                                                             tech.id = stringr::str_replace(string = y$technique, pattern = "Technique/", replacement = ""),
-#                                                             stringsAsFactors = F)
-#                                                }
-#                                              }
-#                                  )
-#                                })
-#   car.url <- "https://car.mitre.org/wiki/Full_Analytic_List"
-#   car.html <- RCurl::getURL(url = car.url, ssl.verifypeer = FALSE)
-#   doc <- xml2::read_html(car.html)
-#
-#   car.analytics <- dplyr::left_join(x = car.analytics,
-#                                     y = as.data.frame(cbind(car.id = rvest::html_text(rvest::xml_nodes(doc,
-#                                                                                                        xpath = '//div[@id="mw-content-text"]/table/tr/td[@class="Analytic smwtype_wpg"]/a/@title')),
-#                                                             car.hypothesis = rvest::html_text(rvest::xml_nodes(doc,
-#                                                                                                                xpath = '//div[@id="mw-content-text"]/table/tr/td[@class="Hypothesis smwtype_txt"]'))),
-#                                                       stringsAsFactors = F),
-#                                     by = c("id" = "car.id"))
-#
-#   # Extract relations
-#   df <- data.frame(from = character(),
-#                    to = character(),
-#                    source = character(),
-#                    target = character(),
-#                    info = character(),
-#                    stringsAsFactors = FALSE)
-#
-#   # Extract relations CAR - tactic
-#   car2tactic <- unique(dplyr::select(car.analytics, id, tactic.name, cover))
-#
-#   # Extract relations CAR - technique
-#   car2tech <- unique(dplyr::select(car.analytics, id, tech.id, cover))
-#
-#   # Tidy data
-#
-#
-#   caret <- list(data.model = car.data.model,
-#                 sensors = car.sensors,
-#                 analytics = car.analytics)
-#
-#   return(caret)
-# }
-#
-# getTechniqueWikiInfo <- function(tech.url = "https://attack.mitre.org/wiki/Technique/T1156") {
-#   # Parse all techniques wiki info
-#   # tech.extra <- rvest::html_nodes(x = tech.doc, xpath = '//div[@id="toc"]')
-#   #
-#   # # Get TOC info
-#   # if (length(tech.extra > 1)) {
-#   #   columns <- rvest::html_text(rvest::html_nodes(x = tech.doc, xpath = '//div[@id="toc"]/ul/li/a/span[2]'))
-#   # } else {
-#   #   columns <- c()
-#   # }
-#
-#   # Get Wiki info
-#   tech.doc <- xml2::read_html(tech.url)
-#   content <- rvest::html_nodes(x = tech.doc, xpath = '//div[@id="content"]')
-#   headlines <- rvest::html_nodes(content, xpath = "//*[self::h1 or self::h2]")
-#   xpath <- sprintf("//p[count(preceding-sibling::h2)=%d]", seq_along(headlines)-1)
-#   techwiki <- purrr::map(xpath, ~rvest::html_nodes(x = content, xpath = .x)) %>% # Get the text inside the headlines
-#     purrr::map(rvest::html_text, trim = TRUE) %>% # get per node in between
-#     purrr::map_chr(paste, collapse = "\n") %>% # collapse the text inbetween
-#     purrr::set_names(headlines %>% rvest::html_node("span") %>% rvest::html_text())
-#   techwiki <- as.data.frame(t(techwiki[techwiki != ""]))
-#   names(techwiki)[1] <- "Description"
-#   id <- stringr::str_split(string = tech.url, pattern = "/")[[1]]
-#   techwiki$id <- id[length(id)]
-#
-#   return(techwiki)
-#
-# }
+  # CAR - Techniques
+  r <- unique(dplyr::select(car, id, tech.id, coverage))
+  names(r) <- c("from", "to", "info")
+  r$info <- as.character(sapply(r$info,
+                                function(x)
+                                  jsonlite::toJSON(paste("[coverage:", x, "]", sep = ""))
+  )
+  )
+  r$source <- rep("CAR", nrow(r))
+  r$target <- rep("Technique", nrow(r))
+
+  df <- dplyr::bind_rows(df, r)
+
+  # CAR - Tactics
+  tactics.url <- "https://attack.mitre.org/wiki/Category:Tactic"
+  doc <- xml2::read_html(tactics.url)
+
+  t.list <- rvest::html_nodes(x = doc, css = "#mw-pages > div > div > div > ul > li > a")
+  tactics <- as.data.frame(t(as.matrix(as.data.frame(rvest::html_attrs(t.list)))),
+                           row.names = FALSE, stringsAsFactors = FALSE)
+  tactics$id <- sapply(stringr::str_split(tactics$href, "/"), function(x) x[3])
+  tactics <- dplyr::select(tactics, id, title)
+
+  r <- unique(dplyr::select(car, id, tactic.name, coverage))
+  r <- dplyr::left_join(r, tactics, by = c("tactic.name" = "title"))
+  names(r) <- c("from", "tactic.name", "info", "to")
+  r$tactic.name <- NULL
+  r$info <- as.character(sapply(r$info,
+                                function(x)
+                                  jsonlite::toJSON(paste("[coverage:", x, "]", sep = ""))
+  )
+  )
+  r$source <- rep("CAR", nrow(r))
+  r$target <- rep("Tactic", nrow(r))
+
+  df <- dplyr::bind_rows(df, r)
+
+  return(df)
+}
+
